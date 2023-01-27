49c2c6cf30c6bde6a22ba0697f4d37d301a6d8d2
EVD <edouard.vieillard@outlook.fr>
2023-01-27 12:23:21 +0100

    Init map

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {Map} from './components/Map';
 import './style/index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Map position={[37.763027, -122.487701]}/>
 );
 
 // If you want to start measuring performance in your app, pass a function
