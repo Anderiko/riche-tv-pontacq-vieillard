44d77ac1a4ecbfe49fdcd205e0f76d7782007628
EVD <edouard.vieillard@outlook.fr>
2023-02-09 09:52:58 +0100

    Merge errors fix

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,11 @@ function App() {
   return (
     <div className="App">
       <div className="map-container">
-        <Map position={[37.33462649252493, -122.00897348937217]}/>
+        <Map labels={[
+            { name: 'San Francisco', lat: 37.7749, lng: -122.4194 },
+            { name: 'Los Angeles', lat: 34.0522, lng: -118.2437 },
+            { name: 'New York', lat: 40.7128, lng: -74.0060 },
+        ]}/>
       </div>
       <div className="chat-container">
         <Chat/>
